d5bb734485f10a9d3a82d301725575d796ce3517
Bartek Janowski <detnotatka@gmail.com>
2023-02-03 19:31:59 +0100

    Big css changes

--- src/App.tsx
+++ src/App.tsx
@@ -7,31 +7,16 @@ import React from 'react';
 import './App.css';
 import Footer from "./components/fragments/footer";
 import Navigation from "./components/fragments/navigation";
+import MainContent from "./components/MainContent";
 
 function App() {
   return (
       <Router>
         <div>
           <Navigation  />
-          {/*<Routes>*/}
-          {/*  <Route exact path="/login"  render={(props) => (<LoginForm />)} element={<LoginForm handleLogin={handleLogin} />} />*/}
-
-          {/*  <Route exact path="/" element={<MainContent />} />*/}
-          {/*  <Route exact path="/ProductModel" element={<ProductModelList />} />*/}
-          {/*  <Route exact path="/ProductModel/details/:IDproduct" element={< ProductModelDetails />  } />*/}
-          {/*  <Route exact path="/ProductModel/add" element={< ProductModelForm />  } />*/}
-          {/*  <Route exact path="/ProductModel/edit/:IDproduct" element={< ProductModelForm />  } />*/}
-
-          {/*  <Route exact path="/Order" element={<OrderList />} />*/}
-          {/*  <Route exact path="/Order/add" element={<OrderForm />} />*/}
-          {/*  <Route exact path="/Order/edit/:IDorder" element={<OrderForm />} />*/}
-          {/*  <Route exact path="/Order/details/:IDorder" element={<OrderDetails />} />*/}
-
-          {/*  <Route exact path="/OrderedProducts" element={<OrderedProductsList />} />*/}
-          {/*  <Route exact path="/OrderedProducts/add" element={<OrderedProductsForm />} />*/}
-          {/*  <Route exact path="/OrderedProducts/edit/:IDordered" element={<OrderedProductsForm />} />*/}
-          {/*  <Route exact path="/OrderedProducts/details/:IDordered" element={< OrderedProductsDetails />  } />*/}
-          {/*</Routes>*/}
+          <Routes>
+            <Route path="/" element={<MainContent />} />
+          </Routes>
           <Footer />
         </div>
       </Router>
