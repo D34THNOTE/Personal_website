aab5557be09c8e10a29c344001cc14a41dcdfbe7
Bartek Janowski <detnotatka@gmail.com>
2023-02-04 20:58:23 +0100

    Added useLocation to navbar

--- src/components/fragments/navigation.tsx
+++ src/components/fragments/navigation.tsx
@@ -1,13 +1,15 @@
 import React from "react"
-import {Link} from "react-router-dom"
+import {Link, useLocation} from "react-router-dom"
 
 function Navigation() {
+    let location = useLocation()
+
     return (
         <nav>
             <ul>
-                <li><Link to="/About">About</Link></li>
-                <li><Link to="/SkillsAndProjects">Skills and projects</Link></li>
-                <li><Link to="/Contact">Contact</Link></li>
+                <li><Link to="/About" className={location.pathname === "/About" ? "active" : ""} >About</Link></li>
+                <li><Link to="/SkillsAndProjects" className={location.pathname === "/SkillsAndProjects" ? "active" : ""} >Skills and projects</Link></li>
+                <li><Link to="/Contact" className={location.pathname === "/Contact" ? "active" : ""} >Contact</Link></li>
             </ul>
         </nav>
     )
