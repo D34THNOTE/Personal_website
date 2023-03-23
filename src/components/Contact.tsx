49a6ebe25a735a8b190568ff01766bc697b699ea
Bartosz Janowski <detnotatka@gmail.com>
2023-03-23 01:09:35 +0100

    Removed Discord information

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,8 +1,7 @@
 import React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-    faGithub,
-    faDiscord
+    faGithub
 } from "@fortawesome/free-brands-svg-icons";
 
 function MainContent() {
@@ -12,18 +11,11 @@ function MainContent() {
                 My e-mail address: bartoszjanowskics@gmail.com
             </h2>
             <h2>
-                You can also reach me on Discord and check out my Github profile!
-            </h2>
-            <h2>
-                (If the Discord link doesn't work despite every thread on the internet claiming one like that should work,
-                you can find me on Discord using this: D34THNOTE#9322)
+                You can also check out my Github profile using the link below!
             </h2>
             <div className="iconActive">
                 <a href="https://github.com/D34THNOTE" className="github" target="_blank">
                     <FontAwesomeIcon icon={faGithub} size="3x" />
-                </a>
-                <a href="https://discord.com/users/417950493772480515" className="discord" target="_blank">
-                    <FontAwesomeIcon icon={faDiscord} size="3x" />
                 </a>
             </div>
 
